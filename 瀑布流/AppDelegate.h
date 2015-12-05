a39f3423afa4bae3d82f389d963ace4a758043fe
HYS <hys@hysdemacbook-pro.local>
2015-12-05 19:02:05 +0800

    Initial Commit

---
+++ 瀑布流/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  瀑布流
+//
+//  Created by HYS on 15/12/5.
+//  Copyright © 2015年 HYS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 瀑布流/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  瀑布流
+//
+//  Created by HYS on 15/12/5.
+//  Copyright © 2015年 HYS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
